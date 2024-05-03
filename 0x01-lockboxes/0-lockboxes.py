918c39145653352c9884988c76c17c926d8f9d32
John Maleek <jmzoran01@gmail.com>
2024-05-03 01:47:40 +0100

    first commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""
-    determine if all the boxes can be opened
-"""
+"""determine if all the boxes can be opened"""
 
 
 def canUnlockAll(boxes):
     """
     Return: True or False
     """
-    n = len(boxes)
-    if n == 0:
-        return True
-    
-    unlocked = {0}
+    a = len(boxes)
     stack = [0]
-    
-    while stack:
-        curr_box = stack.pop()
-        for key in boxes[curr_box]:
-            if key < n and key not in unlocked:
-                unlocked.add(key)
-                stack.append(key)
+    unlocked_box = [1] + [0] * (a - 1)
+    i = 0
 
-    return len(unlocked) == n
+    if a == 0:
+        return True
+    while stack:
+        j = stack.pop()
+        for index in boxes[j]:
+            if index > 0 and index < a and unlocked_box[index] == 0:
+                unlocked_box[index] = 1
+                stack.append(index)
+        i = i + 1
+    if 0 in unlocked_box:
+        return False
+    return True
